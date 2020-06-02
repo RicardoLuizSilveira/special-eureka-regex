74a3a89353a7a0e76e4dae5787f7594371cd4d12
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2020-06-01 22:47:33 -0300

    Desafio do CPF e telefone

--- quantificadores/DesafioCPF.js
+++ quantificadores/DesafioCPF.js
@@ -2,3 +2,5 @@ const texto = `CPF dos aprovados:
 - 658.587.258-69
 - 698.874.412-25
 - 236.987.258-65`
+
+console.log(texto.match(/\d{3}.\d{3}.\d{3}-\d{2}/g))
--- quantificadores/DesafioTelefone.js
+++ quantificadores/DesafioTelefone.js
@@ -1,3 +1,5 @@
 const texto = `Lista telefônica:
 - (11) 98745-6325
--5874-1236`
+-55874-1236`
+
+console.log(texto.match(/[(\d{2})]*\s?\d{5}-\d{4}/g))
