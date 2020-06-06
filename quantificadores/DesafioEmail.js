4147c180be4036c37e1181edd35d4277009e95d2
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2020-06-06 20:12:46 -0300

    Desafio do email

--- quantificadores/DesafioEmail.js
+++ quantificadores/DesafioEmail.js
@@ -1,3 +1,10 @@
 const texto = `Os emails dos convidados são:
 - g.freeman@blackmesa.com
-- dog@gmail.com.br`
+- dog@gmail.com.br
+- xico@gmail.com
+- joao@empresa.info.br
+- maria.silva@registro.br
+rafa.sampaio@yahoo.com
+`
+
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}\.?\w{0,2}/g))
